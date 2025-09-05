a900cb19f5c520325a43e3a29a7e343d19ba7020
Erik Fink <efink14@gmail.com>
2025-09-04 22:17:02 -0400

    Basic Setup

---
+++ src/Assets/index.js
@@ -0,0 +1,62 @@
+import Allergy from './SVGs/Allergy.svg';
+import BackArrow from './SVGs/BackArrow.svg';
+import Dining from './SVGs/Dining.svg';
+import Drinks from './SVGs/Drinks.svg';
+import NoDrinks from './SVGs/NoDrinks.svg';
+import MenuBook from './SVGs/MenuBook.svg';
+import Pending from './SVGs/Pending.svg';
+import Verified from './SVGs/Verified.svg';
+import Donate from './SVGs/Donate.svg';
+import Heart from './SVGs/Heart.svg';
+import EcstaicFace from './SVGs/EcstaicFace.svg';
+import Skull from './SVGs/Skull.svg';
+import SadFace from './SVGs/SadFace.svg';
+import NuetralFace from './SVGs/NuetralFace.svg';
+import Smiley from './SVGs/Smiley.svg';
+import Rocket from './SVGs/Rocket.svg';
+import ThumbsUp from './SVGs/ThumbsUp.svg';
+import Person from './SVGs/Person.svg';
+import Slider from './SVGs/Slider.svg';
+import Timeline from './SVGs/Timeline.svg';
+import Filter from './SVGs/Filter.svg';
+import More from './SVGs/More.svg';
+import LogOut from './SVGs/LogOut.svg';
+import Star from './SVGs/Star.svg';
+import Settings from './SVGs/Settings.svg';
+import Search from './SVGs/Search.svg';
+import Menu from './SVGs/Menu.svg';
+import Home from './SVGs/Home.svg';
+
+export {
+  Allergy,
+  BackArrow,
+  Dining,
+  Drinks,
+  NoDrinks,
+  MenuBook,
+  Pending,
+  Verified,
+  Donate,
+  Heart,
+  EcstaicFace,
+  Skull,
+  SadFace,
+  NuetralFace,
+  Smiley,
+  Rocket,
+  ThumbsUp,
+  Person,
+  Slider,
+  Timeline,
+  Filter,
+  More,
+  LogOut,
+  Star,
+  Settings,
+  Search,
+  Menu,
+  Home,
+};
+
+// Icons used on the MainScreen bottom ribbon. Add/remove here to update UI.
+export const mainScreenIcons = [Home, Rocket, Person, Heart, MenuBook];
